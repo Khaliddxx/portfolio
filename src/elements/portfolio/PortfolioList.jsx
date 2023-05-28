dde8bba8fb3416b55711a9069384e40219c32b01
Khaliddxx <doodz@aucegypt.edu>
2023-05-28 16:48:16 +0800

    update

--- src/elements/portfolio/PortfolioList.jsx
+++ src/elements/portfolio/PortfolioList.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const PortfolioListContent = [
   {
-    image: "image-22",
-    category: "Web Development | Web Design",
-    title: "GPT-4 Article Summarizer",
-    techs: "Vite | React | Tailwind | RapidAPI | Redux",
-    link: "https://gpt4sum.khalidxah.tech/",
-  },
-  {
     image: "image-21",
     category: "Web Development | Web Design",
     title: "Assistio",
     techs: "React | AWS | Figma",
     link: "https://main.df2af5sh88023.amplifyapp.com/",
+  },
+  {
+    image: "image-22",
+    category: "Web Development | Web Design",
+    title: "GPT-4 Article Summarizer",
+    techs: "Vite | React | Tailwind | RapidAPI | Redux",
+    link: "https://gpt4sum.khalidxah.tech/",
   },
   {
     image: "image-20",
